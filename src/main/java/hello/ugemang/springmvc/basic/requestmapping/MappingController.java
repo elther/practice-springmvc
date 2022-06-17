b075e34aa51ddd45f4bb432b9639e541b22a016e
elther <elther@naver.com>
2022-06-17 23:38:49 +0900

    practice : mapping type

--- src/main/java/hello/ugemang/springmvc/basic/requestmapping/MappingController.java
+++ src/main/java/hello/ugemang/springmvc/basic/requestmapping/MappingController.java
@@ -2,6 +2,7 @@ package hello.ugemang.springmvc.basic.requestmapping;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -64,7 +65,7 @@ public class MappingController {
      * @return
      */
 
-    @PostMapping(value = "/mapping-consume", consumes = "application/json")
+    @PostMapping(value = "/mapping-consume", consumes = MediaType.APPLICATION_JSON_VALUE)
     public String mappingConsumes(){
         log.info("mappingConsumes");
         return "ok";
@@ -75,7 +76,7 @@ public class MappingController {
      * @return
      */
 
-    @PostMapping(value = "/mapping-produce", produces = "text/html")
+    @PostMapping(value = "/mapping-produce", produces = MediaType.TEXT_HTML_VALUE)
     public String mappingProduces(){
         log.info("mappingProduces");
         return "ok";
